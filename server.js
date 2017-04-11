7c82311451bc3ca5de00e428bb1be0a987958399
Judy Yang <judy0821.yang@gmail.com>
2017-04-10 23:08:17 -0700

    more initializing

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 var port = process.env.PORT || 8090;
+var player = 3;
 
 //routes for API
 var router = express.Router();
@@ -17,8 +18,20 @@ router.get('/test', function(req, res) {
 
 router.get('/welcome', function(req, res) {
 	var options = {root: __dirname + '/views/'};
-	res.sendFile('join.html', options);
+	res.sendFile('index.html', options);
 }) 
+
+router.get('/kill', function(req, res) {
+	var options = {root: __dirname + '/views/'};
+	res.sendFile('kill.html', options);
+})
+
+router.post('/killed', function(req, res) {
+	log.console(req.killed);
+	var options = {root: __dirname + '/views/'};
+	if(player == 3)
+		res.sendFile('index.html', options);
+})
 
 //register routes
 app.use('/api', router);
